cde43a266c06be9a1861c858b563c0bfd8cf094d
aaron <1105605424@qq.com>
2025-05-13 19:06:15 +0800

    2nd commit.

---
+++ detection/inference.py
@@ -0,0 +1,92 @@
+import os
+import argparse
+
+import PIL
+import albumentations
+import pandas as pd
+import torch
+from collections import OrderedDict
+
+
+from backbones.caddm import CADDM
+from utils import FolderDataset, Xception
+from utils import Runner
+
+
+
+def get_opts():
+    arg = argparse.ArgumentParser()
+    arg.add_argument(
+        "--your-team-name",
+        type=str,
+    )
+    arg.add_argument(
+        "--data-folder",
+        type=str,
+    )
+    arg.add_argument(
+        "--model-weights",
+        type=str,
+    )
+    arg.add_argument(
+        "--result-path",
+        type=str,
+    )
+    opts = arg.parse_args()
+    return opts
+
+
+def get_dataset(opts):
+    ### tips: customize your transforms
+    import torchvision.transforms as Transforms
+    size = 224
+    transforms = Transforms.Compose([
+        Transforms.Resize((256, 256)),
+        Transforms.CenterCrop((size, size)),
+        Transforms.ToTensor(),
+        Transforms.Normalize([0.5, 0.5, 0.5], [0.25, 0.25, 0.25])
+    ])
+
+    # DO NOT change FolderDataset
+    return FolderDataset(opts.data_folder, transforms) 
+
+
+def get_model_runner(opts, dataset):
+    ### tips: customize your model
+    # import yaml
+    # with open('configs/caddm_test.cfg', 'r') as ymlfile:
+    #     config = yaml.safe_load(ymlfile)
+
+    model = Xception(2)
+    #model.fc = torch.nn.Linear(2048, 1)
+    model.load_state_dict(torch.load('./pretrained_on_celebdf.pth'))
+
+
+    # DO NOT change Runner
+    runner = Runner(model, dataset)
+    return runner
+
+
+if __name__ == "__main__":
+    opts = get_opts()
+    dataset = get_dataset(opts)
+    runner = get_model_runner(opts, dataset)
+    results = runner.run()
+
+    os.makedirs(opts.result_path, exist_ok=True)
+    writer = pd.ExcelWriter(os.path.join(opts.result_path, opts.your_team_name + ".xlsx"))
+    prediction_frame = pd.DataFrame(
+        data = {
+            "img_names": results["predictions"].keys(),
+            "predictions": results["predictions"].values(),
+        }
+    )
+    time_frame = pd.DataFrame(
+        data = {
+            "Data Volume": [len(results["predictions"].keys())],
+            "Time": [results["time"]],
+        }
+    )
+    prediction_frame.to_excel(writer, sheet_name="predictions", index=False)
+    time_frame.to_excel(writer, sheet_name="time", index=False)
+    writer.close()
